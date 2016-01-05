5be069e13bf2ba92a3bf9bf817b6c0fdc75bb96e
Stephen Sugden <me@stephensugden.com>
2016-01-05 15:39:01 +0100

    Update deploy script
    Official SDK now supports API Gateway, hooray!

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,7 +1,6 @@
 import {readFileSync} from 'fs'
 
-import {Lambda} from 'aws-sdk'
-import getOrCreateApi from 'api-gateway-tools'
+import {Lambda, APIGateway} from 'aws-sdk'
 import hat from 'hat'
 
 async function main ({
@@ -11,6 +10,8 @@ async function main ({
   apiName,
   lambdaName
 }) {
+  const apiGateway = new APIGateway({region})
+
   // API Gateway APIs always have a deployment stage in the path. By setting the
   // deployment stage to 'spaces' we end up with a URL path identical to the
   // Contentful Delivery API: /spaces/{spaceId}/entries
@@ -26,61 +27,138 @@ async function main ({
 
   console.log('Created lambda function', lambdaName)
 
-  const api = await getOrCreateApi(region, apiName)
+  const restApi = await getOrCreateRestApi(apiGateway, apiName)
 
   console.log('Created API', apiName)
 
-  const resource = await api.getOrCreateResource(resourcePath)
+  const resource = await getOrCreateResourceAtPath(apiGateway, restApi.id, resourcePath)
 
-  console.log('Created Resource', resourcePath)
+  console.log('Created Resource', resource)
 
-  const method = await resource.updateMethod('GET', {
+  try {
+    await p(cb => apiGateway.deleteMethod({
+      httpMethod: 'GET',
+      resourceId: resource.id,
+      restApiId: restApi.id
+    }, cb))
+  } catch (_) {
+  }
+
+  const method = await p(cb => apiGateway.putMethod({
+    authorizationType: "NONE",
     httpMethod: 'GET',
     apiKeyRequired: false,
-    authorizationType: "NONE",
+    resourceId: resource.id,
+    restApiId: restApi.id,
     requestParameters: {
       'method.request.header.Authorization': false,
       'method.request.path.spaceId': false,
     }
-  })
+  }, cb))
 
-  console.log('Created Method GET', resourcePath)
+  console.log('Created Method GET', resource.path)
 
-  const response = await method.updateResponse(200, {
-    modelNames: {},
+  const response = await p(cb => apiGateway.putMethodResponse({
+    httpMethod: 'GET',
+    resourceId: resource.id,
+    restApiId: restApi.id,
+    statusCode: '200',
+    responseModels: {},
     responseParameters: {
       'method.response.header.Access-Control-Allow-Origin': false
     }
-  })
+  }, cb))
 
-  await method.updateIntegration({
+  await p(cb => apiGateway.putIntegration({
+    httpMethod: 'GET',
+    resourceId: resource.id,
+    restApiId: restApi.id,
     type: 'AWS',
-    httpMethod: 'POST',  // <-- Lambdas **always** take a POST
-    uri: `arn:aws:apigateway:${region}:lambda:path/2015-03-31/functions/${lambdaArn}/invocations`,
     credentials: null,
+    integrationHttpMethod: 'POST',  // <-- Lambdas **always** take a POST
+    requestParameters: {},
     requestTemplates: {
       'application/json': `{
         "spaceId": "$input.params('spaceId')",
         "query": "$input.params().querystring",
         "authorization": "$input.params('Authorization')"
       }`
-    }
-  })
+    },
+    uri: `arn:aws:apigateway:${region}:lambda:path/2015-03-31/functions/${lambdaArn}/invocations`
+  }, cb))
 
-  await method.updateIntegrationResponse(200, {
+  await p(cb => apiGateway.putIntegrationResponse({
+    httpMethod: 'GET',
+    restApiId: restApi.id,
+    resourceId: resource.id,
     selectionPattern: null,
+    statusCode: '200',
     responseTemplates: { 'application~1json': '' },
     responseParameters: {
       'method.response.header.Access-Control-Allow-Origin': "'*'"
     }
-  })
+  }, cb))
 
-  const methodArn = method.arn(region, accountId, '*')
+  const methodArn = [
+    'arn:aws:execute-api', 
+    region,
+    accountId,
+    restApi.id + '/*/GET' + resource.path.replace(/\{[^\/\}]+\}/g, '*')
+  ].join(':')
+
   console.log(`Granting invoke permission to ${methodArn}`)
   await grantInvokePermission(lambdaArn, methodArn)
 
-  await api.createDeployment(deploymentStage)
-  console.log(`Deployed https://${api.id}.execute-api.${region}.amazonaws.com/${deploymentStage}${resourcePath}`)
+  const deployment = await p(cb => apiGateway.createDeployment({
+    restApiId: restApi.id,
+    stageName: deploymentStage
+  }, cb))
+
+  console.log(`Deployed https://${restApi.id}.execute-api.${region}.amazonaws.com/${deploymentStage}${resourcePath}`)
+}
+
+async function getOrCreateRestApi (apiGateway, name) {
+  const apis = (await p(cb => apiGateway.getRestApis({}, cb))).items.filter(api => api.name == name)
+  if (apis.length == 0) {
+    return p(cb => apiGateway.createRestApi({name}, cb))
+  } else {
+    return apis[0]
+  }
+}
+
+async function getOrCreateResourceAtPath (apiGateway, restApiId, path) {
+  const pathParts = path.split('/').slice(1)
+
+  const existingResources = (
+    await p(cb => apiGateway.getResources({restApiId}, cb))
+  ).items.reduce(
+    (map, resource) => map.set(resource.path, resource),
+    new Map()
+  )
+
+  for (let i = 0, len = pathParts.length; i < len; i++) {
+    let pathPart = pathParts[i]
+    let subPath = '/' + pathParts.slice(0, i + 1).join('/')
+    let parentPath = '/' + pathParts.slice(0, i).join('/')
+
+    let parentResource = existingResources.get(parentPath)
+    let existingResource = existingResources.get(subPath)
+
+    if (!(parentResource && parentResource.id)) {
+      throw new Error(`Parent Resource ${parentPath} must be created before ${subPath}`)
+    }
+
+    if (!existingResource) {
+      let newResource = await p(cb => apiGateway.createResource({
+        pathPart,
+        restApiId,
+        parentId: parentResource.id,
+      }, cb))
+      existingResources.set(newResource.path, newResource)
+    }
+  }
+
+  return existingResources.get(path)
 }
 
 async function createOrUpdateLambda (region, Role, FunctionName, ZipFile) {
